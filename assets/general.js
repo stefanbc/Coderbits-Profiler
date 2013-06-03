6ec501967575d5f9656aaede7d8d282f66875bae
stefanbc <stefcosma@gmail.com>
2013-06-04 00:56:51 +0300

    Updated save fields function

--- assets/general.js
+++ assets/general.js
@@ -7,22 +7,36 @@ function dropField(target, event) {
 }
 
 var $j = jQuery.noConflict();
- 
 $j(document).ready(function() {
 
-	var post_active_fields, post_inactive_fields;
-
-	post_active_fields = $j(".active_fields").find(".field");
-	post_inactive_fields = $j(".inactive_fields").find(".field");
-
     $j("#fields_form").submit(function(){
-    	$j.post("assets/ajax.php", {
+
+    	var post_active_fields = [];
+		var post_inactive_fields = [];
+
+		$j(".active_fields .field").each(function() {
+		    var field = $j(this).getAttribute("data-id");
+		    post_active_fields.push(field);
+		});
+
+		$j(".inactive_fields .field").each(function() {
+		    var field = $j(this).getAttribute("data-id");
+		    post_inactive_fields.push(field);
+		});
+
+		console.log(post_active_fields);
+		console.log(post_inactive_fields);
+
+    	$j.post("../wp-content/plugins/coderbits/assets/ajax.php", {
     		action: "save_fields",
-    		active_fields : active_fields,
-    		inactive_fields : inactive_fields,
+    		active_fields : post_active_fields,
+    		inactive_fields : post_inactive_fields,
     		rand: Math.random()
     	}, function(data){
-
+    		console.log(data);
+    		if (data == 'Yes') {
+    			//location.reload();
+    		}
     	});
 
 	    // Not to post the form physically

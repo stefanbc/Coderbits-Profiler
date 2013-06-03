cbd016648dfe3d2bbb606e2ac39d9635eff26688
stefanbc <stefcosma@gmail.com>
2013-06-04 01:47:52 +0300

    Removed jQuery noConflict from main js file

--- assets/general.js
+++ assets/general.js
@@ -6,34 +6,35 @@ function dropField(target, event) {
     target.appendChild(document.getElementById(field)); 
 }
 
-var $j = jQuery.noConflict();
-$j(document).ready(function() {
+(function($) {
+	$(document).ready(function() {
 
-    $j("#fields_form").submit(function(){
+	    $("#fields_form").submit(function(){
 
-    	var post_active_fields = $j(".active-fields").children('span').map(function() {
-		    return $j(this).attr("id");
-		}).get().join(",");
+	    	var post_active_fields = $(".active-fields").children('span').map(function() {
+			    return $(this).attr("id");
+			}).get().join(",");
 
-		var post_inactive_fields = $j(".inactive-fields").children('span').map(function() {
-		    return $j(this).attr("id");
-		}).get().join(",");
+			var post_inactive_fields = $(".inactive-fields").children('span').map(function() {
+			    return $(this).attr("id");
+			}).get().join(",");
 
-    	$j.post("../wp-content/plugins/coderbits/assets/ajax.php", {
-    		action: "save_fields",
-    		active_fields : post_active_fields,
-    		inactive_fields : post_inactive_fields,
-    		rand: Math.random()
-    	}, function(data){
-    		if (data == 'Yes') {
-    			$j('.zone-title-fields').text('Fields updated!');
-				setTimeout(function(){
-					$j('.zone-title-fields').text('Fields');    
-				}, 2500);
-    		}
-    	});
+	    	$.post("../wp-content/plugins/coderbits/assets/ajax.php", {
+	    		action: "save_fields",
+	    		active_fields : post_active_fields,
+	    		inactive_fields : post_inactive_fields,
+	    		rand: Math.random()
+	    	}, function(data){
+	    		if (data == 'Yes') {
+	    			$('.zone-title-fields').text('Fields updated!');
+					setTimeout(function(){
+						$('.zone-title-fields').text('Fields');    
+					}, 2500);
+	    		}
+	    	});
 
-		return false;
-    });
-
-});
+			return false;
+	    });
+	    
+	});
+})(jQuery)

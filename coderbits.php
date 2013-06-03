fe0b44ec97687e5f4d869fb3ae0b4c0fe177b9f4
stefanbc <stefcosma@gmail.com>
2013-06-03 11:06:51 +0300

    Updated UI

--- coderbits.php
+++ coderbits.php
@@ -1,13 +1,13 @@
 <?php
     /*
         Plugin Name: Coderbits Profiler
-		Plugin URI: https://github.com/stefanbc/Coderbits-Profiler
-		Description: Made for Wordpress
-		Version: 0.1
-		Author: Stefan Cosma
-		Author URI: http://coderbits.com/stefanbc
-		License: MIT
-	*/
+        Plugin URI: https://github.com/stefanbc/Coderbits-Profiler
+        Description: Made for Wordpress
+        Version: 0.1
+        Author: Stefan Cosma
+        Author URI: http://coderbits.com/stefanbc
+        License: MIT
+    */
     
     add_action('admin_menu','coderbits_profiler');
 
@@ -18,22 +18,38 @@
     function coderbits_profiler_options(){
         global $wpdb;
 
+        // Styling
         echo '<link href="//fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet" type="text/css">';
         echo '<link href="' . plugins_url( 'assets/style.css' , __FILE__ ) . '" rel="stylesheet" type="text/css">';
+
+        // Start the content part
         echo '<div class="main-wrapper">';
         echo '<span class="main-title"><img class="logo" src="' . plugins_url( 'assets/logo.png' , __FILE__ ) . '" alt="coderbits"> Profiler</span>';
+
+        // The left part
+        echo '<div class="left-wrapper sides">';
+        // The profile part
         echo '<h2>Profile</h2>';
         echo '<form method="post">';
         echo '<div style="padding: 10px 0;">Current set Coderbits Profile: <b>' . get_option('coderbits_username') . '</b></div>';
         echo 'Set Coderbits Profile: <input type="text" name="username" id="username" placeholder="Enter you coderbits username">';
         echo '<input type="submit" name="update_coderbits_profiler" value="Set Profile">';
         echo '</form>';
+
+        // The options part
         echo '<h2>Options</h2>';
         echo '<form method="post">';
         echo '';
         echo '';
         echo '<input type="submit" name="update_coderbits_profiler" value="Save Options">';
         echo '</form>';
+        echo '</div>';
+        
+        // The right part
+        echo '<div class="right-wrapper sides">';
+        echo '<h2>Preview</h2>';
+        echo '</div>';
+
         echo '</div>';
         
         $username = $wpdb->escape($_POST['username']);

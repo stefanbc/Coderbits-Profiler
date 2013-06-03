7c279c69b135bef06de52b759652c9ded9c20300
stefanbc <stefcosma@gmail.com>
2013-06-03 20:44:13 +0300

    Added in main js file save function for fields

--- assets/general.js
+++ assets/general.js
@@ -5,3 +5,18 @@ function dropField(target, event) {
     var field = event.dataTransfer.getData('field');
     target.appendChild(document.getElementById(field)); 
 }
+
+var $j = jQuery.noConflict();
+ 
+$j(document).ready(function() {
+    $j("#fields_form").submit(function(){
+    	$j.post("assets/ajax.php", {
+    		action: "save_fields",
+    	}, function(data){
+
+    	});
+
+	    // Not to post the form physically
+		return false;
+    });
+});

68c37cbb74b28b8003d4d46148ebe0aec9abea3c
stefanbc <stefcosma@gmail.com>
2013-06-04 21:14:49 +0300

    Added plugin styling option

--- coderbits.php
+++ coderbits.php
@@ -37,10 +37,16 @@
 
         // Get the username
         $username = $wpdb->escape($_POST['username']);
+        // Get the plugin styling option
+        $coderbits_profiler_options = $wpdb->escape($_POST['coderbits_profiler_options']);
         
         // Updated the username setting with the current set username
         if($username) {
             update_option('coderbits_profiler_username', $username);
+        }
+        // Updated options
+        if ($coderbits_profiler_options) {
+            update_option('coderbits_profiler_options', $coderbits_profiler_options);
         }
 
         // Styling and scripting
@@ -133,8 +139,8 @@
             echo '<div class="sides">';
                 echo '<h2 class="zone-title-preview">Options <small><i>Change widget settings</i></small></h2>';
                 echo '<form method="post" id="options_form">';
-                echo '<input type="checkbox" name="plugin_styling" id="plugin_styling"/> Use plugin styling?';
-                echo '<div id="options_submit_button" class="submit_button"><input type="submit" name="update_options_coderbits_profiler" id="update_options_coderbits_profiler" value="Update Options"></div>';
+                    echo '<input type="checkbox" name="coderbits_profiler_options" id="coderbits_profiler_options" value="1" ' . checked('1', get_option('coderbits_profiler_options')) . ' /> Use plugin styling?';
+                    echo '<div id="options_submit_button" class="submit_button"><input type="submit" name="update_options_coderbits_profiler" id="update_options_coderbits_profiler" value="Update Options"></div>';
                 echo '</form>';
                 echo '<h2 class="zone-title-preview">Preview Widget <small><i>Preview widget based on your settings</i></small></h2>';
                 coderbits_profiler_output_data();
@@ -201,6 +207,7 @@
         // Get the active fields
         $preview_fields = unserialize(get_option('coderbits_profiler_active_fields'));
         if (!empty($preview_fields)) {
+            echo '<div class="cp_output_wraper">';
             foreach ($preview_fields as $preview_field) {
                 if (!empty($preview_field)) {
                     switch ($preview_field) {
@@ -259,6 +266,7 @@
                     }
                 }
             }
+            echo '</div>';
         } else {
             echo '<div class="row">No data to display, yet!</div>';
         }

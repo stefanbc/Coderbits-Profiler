cddbe5b0132835b1b4b1291e671e4e21eb455349
stefanbc <stefcosma@gmail.com>
2013-06-13 08:52:45 +0300

    Updated UI and fixed error in ajax file

--- assets/ajax.php
+++ assets/ajax.php
@@ -1,25 +1,28 @@
 <?php
-	
-	require_once $_SERVER['DOCUMENT_ROOT'] . 'coderbits-plugin/wp-load.php';
+    $parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
+	require_once ($parse_uri[0] . 'wp-load.php');
 
 	global $wpdb;
 
 	$action = $wpdb->escape($_POST['action']);
 
 	switch ($action) {
+
 		case 'save_fields':
+
 			$active_fields = explode(',',$_POST['active_fields']);
 			$inactive_fields = explode(',',$_POST['inactive_fields']);
 
 			$active = update_option('coderbits_profiler_active_fields', serialize($active_fields));
 			$inactive = update_option('coderbits_profiler_inactive_fields', serialize($inactive_fields));
-
+			
 			if ($active && $inactive) {
 				echo 'Yes';
 			} else {
 				echo 'No';
 			}
-		break;
-	}
 
+		break;
+
+	}
 ?>

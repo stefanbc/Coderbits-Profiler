90fefbd2c61cfe98f5a26ac8c5e8739cca241ef4
stefanbc <stefcosma@gmail.com>
2013-06-14 01:10:00 +0300

    Updated ajax notifications

--- assets/general.js
+++ assets/general.js
@@ -9,15 +9,15 @@ function dropField(target, event) {
 (function($) {
 	$(document).ready(function() {
 		$("#fields_form").submit(function() {
-            
+
 			var post_active_fields = $(".active-fields").children('span').map(function() {
 				return $(this).attr("id");
 			}).get().join(",");
-            
+
 			var post_inactive_fields = $(".inactive-fields").children('span').map(function() {
 				return $(this).attr("id");
 			}).get().join(",");
-            
+
 			$.post("../wp-content/plugins/coderbits-profiler/assets/ajax.php", {
 				action: "save_fields",
 				active_fields: post_active_fields,
@@ -25,25 +25,25 @@ function dropField(target, event) {
 				rand: Math.random()
 			}, function(data) {
 				if (data == 'Yes') {
-					$('#fields_submit_button').append('<span class="update">Fields updated!</span>');
+					$('#fields_submit_button').append('<span class="update">Fields updated! Refresh the page to see the new changes.</span>');
 					setTimeout(function() {
 						$('#fields_submit_button .update').remove();
 					}, 3000);
 				} else {
-                    console.log(data);
+					console.log(data);
 				}
 			});
 			return false;
 		});
-        
-        $("#options_form").submit(function() {
-                
+
+		$("#options_form").submit(function() {
+
 			var post_options_visual_theme = $("#coderbits_profiler_options_visual_theme").map(function() {
 				return $(this).val();
 			}).get().join(",");
-            
-            var post_options = post_options_visual_theme;
-            
+
+			var post_options = post_options_visual_theme;
+
 			$.post("../wp-content/plugins/coderbits-profiler/assets/ajax.php", {
 				action: "save_options",
 				plugin_options: post_options,
@@ -55,11 +55,11 @@ function dropField(target, event) {
 						$('#options_submit_button .update').fadeOut(500).remove();
 					}, 5000);
 				} else {
-                    console.log(data);
+					console.log(data);
 				}
 			});
 			return false;
-		});       
-        
+		});
+
 	});
 })(jQuery)
